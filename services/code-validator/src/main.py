9bfae465d88d93aeb2105f821e30f31fcac598fd
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-02-09 01:42:17 +0100

    Set up logger

---
+++ services/code-validator/src/main.py
@@ -0,0 +1,11 @@
+from loguru import logger
+
+def set_up_logger():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(cur_dir)
+    logs_dir = os.path.join(parent_dir, "logs")
+    os.makedirs(logs_dir, exist_ok=True)
+    log_file_path = os.path.join(logs_dir, "syntax_checks.log")
+    logger.add(log_file_path, rotation="50 MB", enqueue=True)
+
+set_up_logger()
