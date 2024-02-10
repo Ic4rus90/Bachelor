6530fbd7d82fe023fe83a4e10123a0c0019352b7
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-02-10 01:43:32 +0100

    Created the functions to decode and encode code

--- services/code-validator/src/main.py
+++ services/code-validator/src/main.py
@@ -1,5 +1,8 @@
 from loguru import logger
 
+import os
+import base64
+
 def set_up_logger():
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(cur_dir)
@@ -8,4 +11,15 @@ def set_up_logger():
     log_file_path = os.path.join(logs_dir, "syntax_checks.log")
     logger.add(log_file_path, rotation="50 MB", enqueue=True)
 
+def decode_code(encoded_code):
+    try:
+        decoded_code = base64.b64decode(encoded_code.encode('utf-8')).decode('utf-8')
+        return decoded_code
+    except Exception as e:
+        logger.error(f"Error decoding code: {e}")
+        raise HTTPException(status_code=400, detail="Invalid request")
+
+def encode_code(decoded_code):
+    encoded_code = base64.b64encode(decoded_code.encode('utf-8')).decode('utf-8')
+    return encoded_code
 set_up_logger()
