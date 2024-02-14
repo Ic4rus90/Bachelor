9a3ea0f63a4dbc4f2401bd9635389d2ab9d37a52
Tuva Aarseth <tuvaea@uia.no>
2024-02-14 12:25:29 +0100

    auth0 management API

--- services/web-app/src/pages/user-management-page.js
+++ services/web-app/src/pages/user-management-page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Button, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Header from '../components/header'
 import { useAuth0 } from "@auth0/auth0-react";
@@ -7,8 +7,9 @@ import LogoutButton from '../components/logout-button'
 
 export default function UserManagementPage() {
   
-  const { user, isAuthenticated, isLoading } = useAuth0();
-  console.log('Dashboard rendering', { isAuthenticated, isLoading });
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  
+  
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -20,8 +21,8 @@ export default function UserManagementPage() {
         <header className="App-header">
           <Container fluid>
             <h1>Welcome to the Account Page</h1>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
+            Name: <h2>{user.name}</h2>
+            Mail: <p>{user.email}</p>
             <p>You are user number {user.sub}. </p>
             <LogoutButton />
           </Container>
