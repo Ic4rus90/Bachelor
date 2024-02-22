b3081ff744d13ed17601da65017d8804cce2ed6c
Ic4rus90 <104506911+Ic4rus90@users.noreply.github.com>
2024-02-22 23:19:17 +0100

    MVP Dashboard with hardcoded report

--- services/web-app/src/pages/dashboard-page.js
+++ services/web-app/src/pages/dashboard-page.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Container, Row, Col, Button, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Header from '../components/header'
 import { useAuth0 } from "@auth0/auth0-react";
+import DashboardTabs from '../components/dashboard-tabs'
+
 
 export default function Dashboard() {
   
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
   console.log('Dashboard rendering', { isAuthenticated, isLoading });
 
   if (isLoading) {
@@ -15,15 +17,18 @@ export default function Dashboard() {
     // Optionally redirect to login or show a message
     return <div>Please log in to vie the dashboard.</div>;
   }
+
+  
   return (
     isAuthenticated && (
       <div className="Dashboard">
         <Header /> 
         <header className="App-header">
           <Container fluid>
-            <h1>Welcome to the Dashboard Page</h1>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
+            <h1>Your latest vulnerability report</h1>
+            <Container>
+            <DashboardTabs />
+            </Container>
           </Container>
         </header>
       </div>
