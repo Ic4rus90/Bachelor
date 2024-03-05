e285071cf80d86327abd76c3f6b764a482134ee9
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-03-05 20:43:10 +0100

    Moved tasks out of main.py

---
+++ services/prefect/src/tasks.py
@@ -0,0 +1,76 @@
+from logger import logger
+from prefect import task
+from models import TokenRequest, SyntaxCheckRequest, SyntaxCheckResponse, LLMRequest
+from config import TOKEN_VALIDATOR_URL, CODE_VALIDATOR_URL, LLM_URL, REPORT_GENERATOR_URL, REPORT_STORAGE_URL
+
+import requests
+
+
+@task(name="Validate Token")
+def validate_token_task(token: str) -> bool:
+    logger.info("Validating token...")
+    try:
+        token_request = TokenRequest(token=token)
+        response = requests.post(TOKEN_VALIDATOR_URL, json=token_request.model_dump())
+        response.raise_for_status()
+        return True
+    except requests.HTTPError as e:
+        if e.response.status_code == 401:
+            logger.error(f"Invalid token received: {e.response.status_code} - {e.response.text}")
+            raise ValueError("Invalid token received")
+        logger.error(f"Request to token validator failed: {e.response.status_code} - {e.response.text}")
+    except requests.RequestException as e:
+        logger.error(f"Request to token validator failed {e}")
+    except Exception as e:
+        logger.error(f"Token validation failed: {e}")
+    return False
+
+@task(name="Validate Code and Generate Prompt")
+def generate_prompt_code_validator_task(code: str, file_extension: str) -> str:
+    logger.info("Sending code to code validator")
+    try:
+        request_data = SyntaxCheckRequest(file_extension=file_extension, code=code)
+        response = requests.post(CODE_VALIDATOR_URL, json=request_data.model_dump())
+        response.raise_for_status()
+        user_prompt = SyntaxCheckResponse(**response.json()).user_prompt
+        logger.info(f"Received prompt from code-validator: {user_prompt}")
+        return user_prompt
+    except requests.HTTPError as e:
+        if e.response.status_code == 400:
+            logger.error(f"Invalid code received: {e.response.status_code} - {e.response.text}")
+            raise ValueError("Invalid code received")
+        elif e.response.status_code == 422:
+            logger.error(f"Invalid file extension received: {e.response.status_code} - {e.response.text}")
+            raise ValueError("Invalid file extension received")
+        elif e.response.status_code == 500:
+            logger.error(f"Error validating code: {e.response.status_code} - {e.response.text}")
+            raise ValueError("Error validating code")
+        logger.error(f"Request to code validator failed: {e.response.status_code} - {e.response.text}")
+        raise ValueError(f"Error validating code")
+    except requests.RequestException as e:
+        logger.error(f"Request to code validator failed {e}")
+        raise ValueError("Code validation failed due to a network or server error")
+    except Exception as e:
+        logger.error(f"Code validation failed: {e}")
+        raise ValueError("Code validation failed")
+
+@task(name="Call LLM Model")
+def call_llm_task(prompt: str) -> str:
+    logger.info("Calling LLM model to analyze code")
+
+    logger.info("LLM output received")
+    return "LLM output"
+
+@task(name="Generate Report")
+def generate_report_task(llm_output: str) -> str:
+    logger.info("Sending LLM output to generate report summary")
+
+    logger.info("Report summary generated")
+    return "Report summary generated"
+
+@task(name="Store Report")
+def store_report_task(llm_output: str) -> str:
+    logger.info("Sending report to web-app for storage")
+
+    logger.info("Report stored successfully")
+    return "Report stored succesfully"
