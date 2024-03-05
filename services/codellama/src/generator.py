8efb665d6c5a276a989951f77c0c0fb85b15dae8
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-03-05 21:13:54 +0100

    Modified output format of llm and added configuration values to prevent users from being able to modify generation with api call

--- services/codellama/src/generator.py
+++ services/codellama/src/generator.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from fastapi import HTTPException
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from config import model_id, cache_dir_model, cache_dir_tokenizer
+from config import model_id, cache_dir_model, cache_dir_tokenizer, system_prompt, max_new_tokens, max_time, repetition_penalty, typical_p, do_sample
 from encoder import decode_base, encode_base
 from logger import logger
 from models import GenerateRequest
@@ -30,7 +30,6 @@ async def generate_text(request, generate_request: GenerateRequest):
     client_host = request.client.host
     logger.info(f"Received generation request from {client_host}.")
 
-    system_prompt = decode_base(generate_request.system_prompt, client_host)
     user_prompt = decode_base(generate_request.user_prompt, client_host)
 
     # Generation
@@ -51,11 +50,11 @@ async def generate_text(request, generate_request: GenerateRequest):
         start_time = datetime.now()
         output = model.generate(
             input_ids=inputs,
-            max_new_tokens=generate_request.max_new_tokens,
-            max_time=generate_request.max_time,
-            repetition_penalty=generate_request.repetition_penalty,
-            typical_p=generate_request.typical_p,
-            do_sample=False
+            max_new_tokens=max_new_tokens,
+            max_time=max_time,
+            repetition_penalty=repetition_penalty,
+            typical_p=typical_p,
+            do_sample=do_sample
         )
         end_time = datetime.now()
         duration = (end_time - start_time).total_seconds()
@@ -67,14 +66,14 @@ async def generate_text(request, generate_request: GenerateRequest):
         logger.info(f"Generation took {duration} seconds (from {client_host})")
 
         # Encode to base64 before response
-        response = encode_base(tokenizer.decode(output, skip_special_tokens=True))
+        response = encode_base(tokenizer.decode(output))
         logger.info(f"Generation request processed successfully. (from {client_host})")
 
         result = [
             {"input_token_num": input_tokens},
             {"output_token_num": output_tokens},
             {"generation_time": duration},
-            {"response": response}
+            {"llm_output": response}
         ]
         return result
     except Exception as e:
