90ba08a0aff4a63ce8db94f9248e567c7f281141
Tåkelur <91278949+takelur@users.noreply.github.com>
2024-03-07 15:41:58 +0100

    Create config.py

---
+++ services/prefect/src/config.py
@@ -0,0 +1,106 @@
+TOKEN_VALIDATOR_URL = "http://localhost:30001/verify-token"
+CODE_VALIDATOR_URL = "http://localhost:30002/check-syntax"
+LLM_URL = "http://localhost:30003/generate"
+REPORT_GENERATOR_URL = "http://localhost:30004/generate-report"
+REPORT_STORAGE_URL = "http://localhost:3000/addreports"
+
+LLM_SCHEMA = {
+  "type": "object",
+  "required": ["vulnerabilities"],
+  "properties": {
+    "vulnerabilities": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "required": ["cweID", "codeExtract", "vulnSummary"],
+        "properties": {
+          "cweID": {
+            "type": "string",
+            "pattern": "^CWE-\\d+$"
+          },
+          "codeExtract": {
+            "type": "string"
+          },
+          "lineNumber": {
+            "type": "integer"
+          },
+          "vulnSummary": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  }
+}
+
+REPORT_SUMMARY_SCHEMA = {
+  "type": "object",
+  "required": ["vulnerabilities"],
+  "properties": {
+    "vulnerabilities": {
+      "type": "array",
+      "minItems": 1,
+      "items": {
+        "type": "object",
+        "required": ["cweID", "codeExtract", "vulnSummary"],
+        "properties": {
+          "cweID": {
+            "type": "string",
+            "pattern": "^CWE-\\d+$"
+          },
+          "codeExtract": {
+            "type": "string"
+          },
+          "vulnSummary": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  }
+}
+
+REPORT_FULL_SCHEMA = {
+  "type": "object",
+  "required": ["vulnerabilities", "analyzedCode"],
+  "properties": {
+    "userID": {
+      "type": "string"
+    },
+    "vulnerabilities": {
+      "type": "array",
+      "minItems": 1,
+      "items": {
+        "type": "object",
+        "required": ["cweID", "codeExtract", "vulnSummary"],
+        "properties": {
+          "cweID": {
+            "type": "string",
+            "pattern": "^CWE-\\d+$"
+          },
+          "codeExtract": {
+            "type": "string"
+          },
+          "vulnSummary": {
+            "type": "string"
+          }
+        }
+      }
+    },
+    "analyzedCode": {
+      "type": "object",
+      "required": ["code", "language", "startingLineNumber"],
+      "properties": {
+        "code": {
+          "type": "string"
+        },
+        "language": {
+          "type": "string"
+        },
+        "startingLineNumber": {
+          "type": "integer"
+        }
+      }
+    }
+  }
+}
