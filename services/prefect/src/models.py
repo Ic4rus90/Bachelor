0cbf7fd0cca70c208cd12a6fc360ad7a864539fb
Tåkelur <91278949+takelur@users.noreply.github.com>
2024-03-07 15:44:05 +0100

    Create models.py

---
+++ services/prefect/src/models.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+class TokenRequest(BaseModel):
+    token: str
+
+class TokenResponse(BaseModel):
+    user_id: str
+
+class CodeAnalysisRequest(BaseModel):
+    code: str
+    file_extension: str
+    #token: str
+
+class SyntaxCheckRequest(BaseModel):
+    file_extension: str
+    code: str
+
+class SyntaxCheckResponse(BaseModel):
+    user_prompt: str
+
+class LLMRequest(BaseModel):
+    user_prompt: str
+    system_prompt: str = ""
+
+class LLMResponse(BaseModel):
+    input_token_num: int
+    output_token_num: int
+    generation_time: float
+    llm_output: str
+
+class GenerateReportRequest(BaseModel):
+    llm_output: str
+
+class GenerateReportResponse(BaseModel):
+    report_summary: str
+    report_full: str
+
+class StoreReportRequest(BaseModel):
+    report_full: str
+  
