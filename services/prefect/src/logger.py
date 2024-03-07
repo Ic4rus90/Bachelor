0867a0081755daa03c4cd85e68586592af67b81a
Tåkelur <91278949+takelur@users.noreply.github.com>
2024-03-07 15:43:26 +0100

    Create logger.py

---
+++ services/prefect/src/logger.py
@@ -0,0 +1,11 @@
+import os
+from loguru import logger
+
+def set_up_logger():
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(cur_dir)
+    logs_dir = os.path.join(parent_dir, "logs")
+    os.makedirs(logs_dir, exist_ok=True)
+    log_file_path = os.path.join(logs_dir, "workflow.log")
+    logger.add(log_file_path, format="{time:YYYY-MM-DD HH:mm:ss.ms} {extra} {level} {message} ", rotation="50 MB", enqueue=True)
+  
