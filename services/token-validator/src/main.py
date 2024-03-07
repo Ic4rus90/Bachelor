dbd4d14cad46579da03813816d189cd50ee78514
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-03-07 10:26:13 +0100

    Now returns user ID on valid token

--- services/token-validator/src/main.py
+++ services/token-validator/src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from logger import set_up_logger
-from token_validator import verify_token
+from token_validator import verify_token, get_user_id
 from config import JWKS_URL
 from jwks_client import JWKSClient
-from models import GenerateRequest
+from models import GenerateRequest, GenerateResponse
 
 app = FastAPI()
 
@@ -12,11 +12,12 @@ set_up_logger()
 # Initialize JWKSClient
 jwks_client = JWKSClient(jwks_url=JWKS_URL)
 
-@app.post("/verify-token/")
+@app.post("/verify-token/", response_model=GenerateResponse)
 async def verify(request: GenerateRequest):
     is_valid = verify_token(request, jwks_client)
     if is_valid:
-        return {"message": "Token is valid."}
+        user_id = get_user_id(request.token)
+        return GenerateResponse(user_id=user_id)
     else:
         raise HTTPException(status_code=401, detail="Token is invalid.")
 
--- services/token-validator/src/models.py
+++ services/token-validator/src/models.py
@@ -2,3 +2,6 @@ from pydantic import BaseModel
 
 class GenerateRequest(BaseModel):
     token: str
+
+class GenerateResponse(BaseModel):
+    user_id: str
