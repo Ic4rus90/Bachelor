94a1b8464f228177bc62620b4ba8a865a7e38aaf
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-03-08 20:41:53 +0100

    Added error code for too long input (or too much tea x))

--- services/extension/src/send-request.ts
+++ services/extension/src/send-request.ts
@@ -54,6 +54,8 @@ async function getAnalyzedCode(code: string, file_extension: string, token: stri
                     throw new Error('Invalid code received');
                 case 422:
                     throw new Error('Invalid file extension received');
+                case 418:
+                    throw new Error('The code sent for analysis is too long. Please try again with a smaller code snippet.');
                 default:
                     throw new Error('Server error occured. Please contact us for assistance');
             }
