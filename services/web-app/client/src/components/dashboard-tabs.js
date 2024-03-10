c7235cf0289fbc942f07196dbfadeb13241b32b4
Tuva Aarseth <tuvaea@uia.no>
2024-03-10 19:28:50 +0100

    Changed styling of latest security scan, added handling if vuln array is empty, added report timestamp

--- services/web-app/client/src/components/dashboard-tabs.js
+++ services/web-app/client/src/components/dashboard-tabs.js
@@ -5,17 +5,24 @@ import CodeBlock from './display-code';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import React, { useEffect, useState } from 'react';
+import './dashboard-tabs.css'
+import '../pages/dashboard-page.css'
+
 
 
 const DashboardTabs = () => {
   const [reportData, setReportData] = useState(null);
   const { getAccessTokenSilently } = useAuth0();
+  const formatDate = (dateString) => {
+    const options = { month: 'short', day: 'numeric', hour: 'numeric', minute: '2-digit', hour12: false };
+    return new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
+  };
   useEffect(() => {
       const getReports = async () => {
           try {
               const token = await getAccessTokenSilently(); 
               
-              const response = await fetch('http://127.0.0.1:3000/getreports', {
+              const response = await fetch('http://127.0.0.1:3001/getreports', {
                   headers: {
                       Authorization: `Bearer ${token}`,
                   },
@@ -37,21 +44,32 @@ const DashboardTabs = () => {
   if (!reportData) {
     return <div>You have no reports</div>;
   }
-  
+
+
 
   return (
-    <Tabs
-      defaultActiveKey="vulnerabilities"
-      id="Report-tabs"
-      className="mb-3"
-      style={{ color: '#FFFFFF' }}>
-      <Tab eventKey="vulnerabilities" title="Vulnerabilities ">
-        <VulnerabilityCards vulnerabilities={reportData.vulnerabilities} />
-      </Tab>
-      <Tab eventKey="code" title="Code">
-        <CodeBlock codeString={reportData.analyzed_code.code} />
-      </Tab>
-    </Tabs>
+    <>
+      <div className="dashboard-header">
+        <h1 className="dashboard-title">Latest Security Scan Results</h1>
+          {reportData && (
+            <span className="dashboard-timestamp">
+            Scanned: {formatDate(reportData.report.report_date)}
+          </span>
+          )}
+      </div>
+      <Tabs
+        defaultActiveKey="vulnerabilities"
+        id="Report-tabs"
+        className="custom-tabs mb-3"
+        >
+        <Tab eventKey="vulnerabilities" title="Vulnerabilities" className="custom-tab">
+          <VulnerabilityCards vulnerabilities={reportData.vulnerabilities} />
+        </Tab>
+        <Tab eventKey="code" title="Code" className="custom-tab">
+          <CodeBlock codeString={reportData.analyzed_code.code} />
+        </Tab>
+      </Tabs>
+    </>
   );
 };
 
