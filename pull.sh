0ee6c55e7f8511d020131bcba3cb0e4494a6994a
Håvard Fjellbakken <91278949+takelur@users.noreply.github.com>
2024-03-12 16:31:07 +0100

    Initial commit

---
+++ pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git pull
+chgrp bsc-gruppe17-24v /raid/bsc-gruppe17-24v/Bachelor -R
---
+++ services/addalias.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "alias pull='git pull && chgrp bsc-gruppe17-24v /raid/bsc-gruppe17-24v/Bachelor -R'" >> ~/.bashrc
