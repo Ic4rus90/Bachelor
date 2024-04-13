05ccd627b52742a209abe1304683fe31028425c3
Ic4rus90 <104506911+Ic4rus90@users.noreply.github.com>
2024-04-13 18:37:01 +0200

    Removed test function, and updated .vsix

--- services/extension/test.py
+++
@@ -1,9 +0,0 @@
-# Generate a random python function
-# Usage: python test.py
-# Output: random python function
-
-import random
-import string
-
-def random_string(length):
-    return ''.join(random.choice(string.ascii_letters) for i in range(length))
